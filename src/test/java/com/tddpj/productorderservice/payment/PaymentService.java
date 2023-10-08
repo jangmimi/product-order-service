c22f74c5de7a96b539794d2144eef603be29eb34
jangmimi <alwkd0101@naver.com>
2023-10-08 15:18:34 +0900

    API 테스트로 전환

--- src/test/java/com/tddpj/productorderservice/payment/PaymentService.java
+++ src/test/java/com/tddpj/productorderservice/payment/PaymentService.java
@@ -1,9 +1,15 @@
 package com.tddpj.productorderservice.payment;
 
 import com.tddpj.productorderservice.order.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
+@RequestMapping("/payments")
 class PaymentService {
     private final PaymentPort paymentPort;
 
@@ -11,12 +17,15 @@ class PaymentService {
         this.paymentPort = paymentPort;
     }
 
-    public void payment(PaymentRequest request) {
+    @RequestMapping
+    public ResponseEntity<Void> payment(@RequestBody PaymentRequest request) {
         Order order = paymentPort.getOrder(request.orderId());
 
         final Payment payment = new Payment(order, request.cardNumber());
 
         paymentPort.pay(payment.getPrice(), payment.getCardNumber());
         paymentPort.save(payment);
+
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
